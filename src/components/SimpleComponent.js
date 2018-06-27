8e6c1f8a13617057ca88ba9c4c57113d71c42ec4
jessebillard <jessebillard@mac.com>
2018-06-27 16:49:56 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
 // Code SimpleComponentHere Here
+import React from 'react';
+
+class SimpleComponent extends React.Component {
+    constructor() {
+        super() 
+        this.state = {
+            mood: 'happy'
+        }
+    }
+
+    clickHandler = () => {
+        this.setState({
+            mood: this.state.mood === 'happy' ? 'sad' : 'happy'
+        })
+    }
+
+    render() {
+        return (
+            <div onClick={this.clickHandler}>
+                {this.state.mood} 
+            </div>
+        )
+    }
+
+}
+
+export default SimpleComponent;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => console.log("there's a snake in my boot")} />
   </div>,
   document.getElementById('root')
 );
